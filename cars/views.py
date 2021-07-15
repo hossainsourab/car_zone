e9ac8f50e1f219397d75ce6f2fd87be7268921f2
sourab21 <ssourab21@gmail.com>
2021-07-15 07:20:12 +0600

    Search Forms, Search Functionality & Working with Admin Style

--- cars/views.py
+++ cars/views.py
@@ -7,11 +7,21 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def cars(request):
     all_cars = Car.objects.order_by('-created_date')
-    paginator = Paginator(all_cars, 2)
+    paginator = Paginator(all_cars, 4)
     page = request.GET.get('page')
     paged_cars = paginator.get_page(page)
+    # start search function start
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    # end search function start
     data = {
-        'cars': paged_cars
+        'cars': paged_cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
     }
     return render(request, 'cars/cars.html', context=data)
 
@@ -22,3 +32,60 @@ def car_details(request, id):
         'single_car': single_car
     }
     return render(request, 'cars/car_details.html', context=data)
+
+
+def search(request):
+    cars = Car.objects.order_by('-created_date')
+    # start search function start
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    transmission_search = Car.objects.values_list('transmission', flat=True).distinct()
+    # end search function start
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.filter(description__icontains=keyword)
+
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:
+            cars = cars.filter(model__iexact=model)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            cars = cars.filter(city__iexact=city)
+
+    if 'year' in request.GET:
+        year = request.GET['year']
+        if year:
+            cars = cars.filter(year__iexact=year)
+
+    if 'body_style' in request.GET:
+        body_style = request.GET['body_style']
+        if body_style:
+            cars = cars.filter(body_style__iexact=body_style)
+
+    if 'transmission' in request.GET:
+        transmission = request.GET['transmission']
+        if transmission:
+            cars = cars.filter(transmission__iexact=transmission)
+
+    if 'min_price' in request.GET:
+        min_price = request.GET['min_price']
+        max_price = request.GET['max_price']
+        if max_price:
+            cars = cars.filter(price__gte=min_price, price__lte=max_price)
+
+    data = {
+        'cars': cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
+        'transmission_search': transmission_search,
+    }
+    return render(request, 'cars/search.html', context=data)
--- pages/views.py
+++ pages/views.py
@@ -8,10 +8,20 @@ def home(request):
     teams = Team.objects.all()
     featured_car = Car.objects.order_by('-created_date').filter(is_featured=True)
     all_car = Car.objects.order_by('-created_date')
+    # start search function start
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    # end search function start
     data = {
         'teams': teams,
         'featured_car': featured_car,
         'all_car': all_car,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
     }
     return render(request, 'pages/home.html', context=data)
 
